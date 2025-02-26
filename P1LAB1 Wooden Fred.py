dedceef12e4404098e7d0957c19ce44b10b1a2fc
fawooden-cpu <woodenf7509@student.faytechcc.edu>
2025-02-26 16:14:27 -0500

    Add files via upload

---
+++ P1LAB1 Wooden Fred.py
@@ -0,0 +1,13 @@
+#FredWooden
+#02-20-2025
+# Web, Pgm, & Db Foundation (CTI-110-0903)
+# how to create a Python program using IDLE
+
+firstname= input("Enter your first name: ") 
+lastname= input("Enter your last name: ")
+print("Hello," , firstname, lastname + "! Welcome to CTI-110")
+
+
+
+
+
